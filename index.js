3acd52dd32188c5c1633d8fce4548bf53f0647bf
HadiaMushtaq-Dev <hadiamushtaq397@gmail.com>
2025-12-15 23:33:58 +0500

    correct cors

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config()
 app.use(express.json())
 app.use(cors())
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "http://auth-frontend-sigma-steel.vercel.app", 
   credentials: true               
 }));
 app.use(express.urlencoded({extended:true}))
